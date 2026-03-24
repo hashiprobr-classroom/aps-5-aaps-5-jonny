996ee8073054777516529eaf6eacd88fe4acdd53
joao <joaoham@al.insper.edu.br>
2026-03-24 18:04:46 -0300

    Fix: Momento tirando >= para só >

--- freela/src/main/java/Tarefa.java
+++ freela/src/main/java/Tarefa.java
@@ -30,7 +30,7 @@ public class Tarefa {
 
 
     public void atualiza(Momento momento1, Momento momento2){
-        if (momento1.minutos() >= momento2.minutos()) {
+        if (momento1.minutos() > momento2.minutos()) {
             throw new IllegalArgumentException("O início deve ser antes do fim.");
         }
         this.inicio = momento1;
